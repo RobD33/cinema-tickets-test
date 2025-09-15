30a6b51d16bf4b25ab58f90873ecf2e6c496a7aa
Robert Davidson <private@example.com>
2025-09-15 17:44:13 +0100

    chore: move validation tests to ValidateTicketRequest.test.js

--- test/pairtest/TicketService.test.js
+++ test/pairtest/TicketService.test.js
@@ -1,39 +1,20 @@
 import TicketService from '../../src/pairtest/TicketService';
-import InvalidPurchaseException from '../../src/pairtest/lib/InvalidPurchaseException';
 import TicketTypeRequest from '../../src/pairtest/lib/TicketTypeRequest';
+import ValidateTicketRequest from '../../src/pairtest/lib/ValidateTicketRequest';
+
+jest.mock('../../src/pairtest/lib/ValidateTicketRequest');
+
+const validate = jest.spyOn(ValidateTicketRequest.prototype, 'validate');
 
 describe('TicketService', () => {
-  it.each([-1, -10, 0, -999])('throws InvalidPurchaseException if accountId is less than 1 (%s)', (accountId) => {
-    expect.assertions(2);
-    try{
-      new TicketService().purchaseTickets(accountId);
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidPurchaseException);
-      expect(e.message).toEqual('accountId must be positive integer');
-    }
-  });
-
-  it.each(['hello', {}, undefined])('throws InvalidPurchaseException if accountId is not an integer', (accountId) => {
-    expect.assertions(2);
-    try{
-      new TicketService().purchaseTickets(accountId);
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidPurchaseException);
-      expect(e.message).toEqual('accountId must be positive integer');
-    }
-  });
-
-  it.each([
-    [[{}]],
-    [[new TicketTypeRequest('ADULT', 2), 'CHILD']],
-    [[new TicketTypeRequest('ADULT', 1), new TicketTypeRequest('CHILD', 2), []]]
-  ])('Throws an InvalidPurchaseException if passed anything other than accountId and TicketTypeRequests', (ticketTypeRequests) => {
-    expect.assertions(2);
-    try{
-      new TicketService().purchaseTickets(1, ...ticketTypeRequests);
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidPurchaseException);
-      expect(e.message).toEqual('All arguments after accountId must be of type TicketTypeRequest');
-    }
+  it('Creates ValidateTicketRequest with accountId and ticketTypeRequests, and calls validate()', () => {
+    const accountId = 12345678;
+    const ticketTypeRequests = [
+      new TicketTypeRequest('ADULT', 4),
+      new TicketTypeRequest('CHILD', 3),
+    ];
+    new TicketService().purchaseTickets(accountId, ...ticketTypeRequests);
+    expect(ValidateTicketRequest).toHaveBeenCalledWith(accountId, ticketTypeRequests);
+    expect(validate).toHaveBeenCalledTimes(1);
   });
 });
