b2962cd095440417d63216ee4c8fd8cb39cb0200
Robert Davidson <private@example.com>
2025-09-15 18:34:14 +0100

    feat(TicketService.js): calculate number of seats and call reserveSeat

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,10 +1,24 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
 import ValidateTicketRequest from './lib/ValidateTicketRequest.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService';
 
 export default class TicketService {
+  #seatReservationService = new SeatReservationService();
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     new ValidateTicketRequest(accountId, ticketTypeRequests).validate();
+    const numberOfSeats = this.#calculateNumberOfSeats(ticketTypeRequests);
+    this.#seatReservationService.reserveSeat(accountId, numberOfSeats);
+  }
+
+  #calculateNumberOfSeats(ticketTypeRequests) {
+    const tally = {
+      ADULT: 0,
+      CHILD: 0,
+      INFANT: 0,
+    }
+    ticketTypeRequests.forEach(ticketTypeRequest => {
+      tally[ticketTypeRequest.getTicketType()] += ticketTypeRequest.getNoOfTickets()
+    });
+    return tally.ADULT + tally.CHILD;
   }
 }
--- test/pairtest/TicketService.test.js
+++ test/pairtest/TicketService.test.js
@@ -1,12 +1,18 @@
 import TicketService from '../../src/pairtest/TicketService';
 import TicketTypeRequest from '../../src/pairtest/lib/TicketTypeRequest';
 import ValidateTicketRequest from '../../src/pairtest/lib/ValidateTicketRequest';
+import SeatReservationService from '../../src/thirdparty/seatbooking/SeatReservationService';
 
 jest.mock('../../src/pairtest/lib/ValidateTicketRequest');
+jest.mock('../../src/thirdparty/seatbooking/SeatReservationService');
 
 const validate = jest.spyOn(ValidateTicketRequest.prototype, 'validate');
+const reserveSeat = jest.spyOn(SeatReservationService.prototype, 'reserveSeat');
 
 describe('TicketService', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('Creates ValidateTicketRequest with accountId and ticketTypeRequests, and calls validate()', () => {
     const accountId = 12345678;
     const ticketTypeRequests = [
@@ -17,4 +23,24 @@ describe('TicketService', () => {
     expect(ValidateTicketRequest).toHaveBeenCalledWith(accountId, ticketTypeRequests);
     expect(validate).toHaveBeenCalledTimes(1);
   });
+
+  it.each([
+    [[new TicketTypeRequest('ADULT', 4)], 4],
+    [[new TicketTypeRequest('ADULT', 2), new TicketTypeRequest('CHILD', 3)], 5],
+    [[new TicketTypeRequest('ADULT', 4), new TicketTypeRequest('CHILD', 4), new TicketTypeRequest('INFANT', 4)], 8],
+    [[
+      new TicketTypeRequest('ADULT', 4),
+      new TicketTypeRequest('CHILD', 4),
+      new TicketTypeRequest('ADULT', 4),
+      new TicketTypeRequest('CHILD', 4),
+      new TicketTypeRequest('CHILD', 0),
+      new TicketTypeRequest('INFANT', 6),
+    ], 16],
+  ])('Calls SeatReservationService.reserveSeat once with accountId and correct number of seats', (ticketTypeRequests, seats) => {
+    const accountId = 123456;
+    new TicketService().purchaseTickets(accountId, ...ticketTypeRequests);
+    expect(reserveSeat).toHaveBeenCalledWith(accountId, seats);
+    expect(reserveSeat).toHaveBeenCalledTimes(1);
+  });
+
 });
