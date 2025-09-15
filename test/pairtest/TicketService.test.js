684498bb061dfb734824dcca20d4b0273482dc58
Robert Davidson <private@example.com>
2025-09-15 17:12:35 +0100

    feat(TicketService.test.js): add test for type of objects passed for ticketTypeRequests

--- test/pairtest/TicketService.test.js
+++ test/pairtest/TicketService.test.js
@@ -1,5 +1,6 @@
 import TicketService from '../../src/pairtest/TicketService';
 import InvalidPurchaseException from '../../src/pairtest/lib/InvalidPurchaseException';
+import TicketTypeRequest from '../../src/pairtest/lib/TicketTypeRequest';
 
 describe('TicketService', () => {
   it.each([-1, -10, 0, -999])('throws InvalidPurchaseException if accountId is less than 1 (%s)', (accountId) => {
@@ -19,6 +20,20 @@ describe('TicketService', () => {
     } catch (e) {
       expect(e).toBeInstanceOf(InvalidPurchaseException);
       expect(e.message).toEqual('accountId must be positive integer');
+    }
+  });
+
+  it.each([
+    [[{}]],
+    [[new TicketTypeRequest('ADULT', 2), 'CHILD']],
+    [[new TicketTypeRequest('ADULT', 1), new TicketTypeRequest('CHILD', 2), []]]
+  ])('Throws an InvalidPurchaseException if passed anything other than accountId and TicketTypeRequests', (ticketTypeRequests) => {
+    expect.assertions(2);
+    try{
+      new TicketService().purchaseTickets(1, ticketTypeRequests);
+    } catch (e) {
+      expect(e).toBeInstanceOf(InvalidPurchaseException);
+      expect(e.message).toEqual('All arguments after accountId must be of type TicketTypeRequest');
     }
   });
 });
